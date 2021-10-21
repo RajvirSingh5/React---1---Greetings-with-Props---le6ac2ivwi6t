8e0e84b0ed76f0f5b7d66496b51fc29007189e50
RajvirSingh5 <83012541+RajvirSingh5@users.noreply.github.com>
2021-10-21 20:05:43 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,15 @@
 // write code for Welcome component here
+import React from 'react';
+
+ const Welcome = (props) => {
+   //let name = props
+  return(<div>
+  <h1>Hello {props.name} !</h1>
+  <h2> Welcome to Newton School.</h2>
+</div>);
+
+}
+
+Welcome.defaultProps = 'Rajvir';
+
+export default Welcome;
